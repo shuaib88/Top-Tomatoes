d8c2a4ac4c0bfd46222f23cab0c1231f749d2a9a
Shuaib Ahmed <ahmed1@uchicago.edu>
2016-03-17 06:56:40 -0500

    Fixed Text Alignment Issue

---
+++ TopTomatoes/TopTomatoes/TopAllignLabel.swift
@@ -0,0 +1,48 @@
+//
+//  TopAllignLabel.swift
+//  TopTomatoes
+//
+//  Created by Shuaib Ahmed on 3/17/16.
+//  Copyright © 2016 Shuaib Labs. All rights reserved.
+/// - Attributions: http://stackoverflow.com/questions/7192088/how-to-set-top-left-alignment-for-uilabel-for-ios-application
+
+import UIKit
+
+class TopAllignLabel: UILabel {
+    
+    enum VerticalAlignment : Int {
+        case VerticalAlignmentTop = 0
+        case VerticalAlignmentMiddle = 1
+        case VerticalAlignmentBottom = 2
+    }
+    
+    var verticalAlignment : VerticalAlignment = .VerticalAlignmentTop {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    required init(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)!
+    }
+    
+    override func textRectForBounds(bounds: CGRect, limitedToNumberOfLines: Int) -> CGRect {
+        let rect = super.textRectForBounds(bounds, limitedToNumberOfLines: limitedToNumberOfLines)
+        
+        switch(verticalAlignment) {
+        case .VerticalAlignmentTop:
+            return CGRectMake(bounds.origin.x, bounds.origin.y, rect.size.width, rect.size.height)
+        case .VerticalAlignmentMiddle:
+            return CGRectMake(bounds.origin.x, bounds.origin.y + (bounds.size.height - rect.size.height) / 2, rect.size.width, rect.size.height)
+        case .VerticalAlignmentBottom:
+            return CGRectMake(bounds.origin.x, bounds.origin.y + (bounds.size.height - rect.size.height), rect.size.width, rect.size.height)
+        }
+    }
+    
+    override func drawTextInRect(rect: CGRect) {
+        let r = self.textRectForBounds(rect, limitedToNumberOfLines: self.numberOfLines)
+        super.drawTextInRect(r)
+    }
+
+
+}
