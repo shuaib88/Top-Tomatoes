23a9d854e7bd1f816233f551e4f88f225c3fbb1a
Shuaib Ahmed <ahmed1@uchicago.edu>
2016-03-18 05:19:39 -0500

    Attempt to add scrollview

---
+++ TopTomatoes/TopTomatoes/MovieDetailinScrollViewController.swift
@@ -0,0 +1,233 @@
+//
+//  MovieDetailinScrollViewController.swift
+//  TopTomatoes
+//
+//  Created by Shuaib Ahmed on 3/18/16.
+//  Copyright © 2016 Shuaib Labs. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailinScrollViewController: UIViewController {
+    
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var plotSummary: UILabel!
+    
+//    var scrollView : UIScrollView!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    var movietitle: String?
+    var movieimage: UIImage?
+    var imageUrl: String?
+    var plotOverview: String?
+    var voteAverage: String?
+    var posterPath: String?
+    
+    // activity indicator
+    var _activityIndicator: ActivityIndicatorView?
+    
+    // star images
+    let filledStar: UIImage = UIImage(named: "star")!
+    let unfilledStar: UIImage = UIImage(named: "unfilledStar")!
+    
+    // for storing favorites
+    let defaults = NSUserDefaults.standardUserDefaults()
+    var favoritesArray = [[String:AnyObject]]()
+    var passedDetailItem = [String:AnyObject]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        scrollView = UIScrollView(frame: view.bounds)
+//        scrollView.backgroundColor = UIColor.clearColor()
+        scrollView.contentSize.width = 367
+        scrollView.contentSize.height = 667 * 2
+//
+//        scrollView.autoresizingMask = UIViewAutoresizing.FlexibleHeight
+//        
+//        scrollView.addSubview(movieTitle)
+//        scrollView.addSubview(movieImage)
+//        scrollView.addSubview(plotSummary)
+//        
+//        view.addSubview(scrollView)
+        
+        // Do any additional setup after loading the view.
+        
+        if let title = movietitle {
+            movieTitle.text = title
+            movieImage.image = movieimage
+            print(imageUrl)
+            plotSummary.text = plotOverview
+        }
+        
+        // check if favorites array exists, if not -- add it
+        if let favArray = defaults.objectForKey("favoritesArray") as! [[String:AnyObject]]? {
+            favoritesArray = favArray
+        } else {
+            // initializes defaults with an empty array of strings
+            defaults.setObject(favoritesArray, forKey: "favoritesArray")
+        }
+        
+        // check if current item is in favorites
+        if isCurrentMovieInFavorites() {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(image: filledStar, style: .Plain, target: self, action: "addFavorite")
+        } else {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(image: unfilledStar, style: .Plain, target: self, action: "addFavorite")
+        }
+        // add bar button
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let _ = _activityIndicator {
+        } else {
+            self._activityIndicator = ActivityIndicatorView()
+            self.view.addSubview(_activityIndicator!)
+        }
+        print("large image url: \(imageUrl)")
+        // call downloadwithclosure
+        downloadImageWithClosure(imageUrl!)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self._activityIndicator!.removeFromSuperview()
+    }
+    
+    // downloads image using an api call
+    func downloadImageWithClosure(url: String) {
+        let startTime = CFAbsoluteTimeGetCurrent()
+        
+        let nsUrl = NSURL(string: url)
+        
+        // Make the download call
+        downloadImage(nsUrl!) { (downloadedImage) -> () in
+            // Ensure the completion block is on the main thread
+            sleep(1)
+            self.movieImage.image = downloadedImage
+            print("Elaspsed Time: " + (NSString(format: "%2.5f", CFAbsoluteTimeGetCurrent() - startTime) as String))
+        }
+    }
+    // helper function that actually opens an NSURL session
+    func downloadImage(url: NSURL, completion: (UIImage)->()) {
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
+            
+            // Check that there was no data; this is misleading since a 404 does not
+            // return an error.  Note: All paramters in the completion handler are optionals
+            guard error == nil else {
+                print("error: \(error!.localizedDescription): \(error!.userInfo)")
+                return
+            }
+            
+            guard response != nil else {
+                // Check the response
+                print("Response: \(response)")
+                return
+            }
+            
+            guard let taskData: NSData = data where data != nil else {
+                print("Error with data")
+                return
+            }
+            
+            // Create a UIImage from the response data and pass it to the completion
+            // handler
+            //print(taskData)
+            if let image = UIImage(data: taskData) {
+                print("Done downloading")
+                dispatch_async(dispatch_get_main_queue()) {
+                    completion(image)
+                }
+            }
+        })
+        
+        // Tasks start in the suspended state, so resume it to start immediately
+        task.resume()
+    }
+    
+    // This method managers the favorite star showing + updating what is stored in the favorites array
+    func addFavorite() {
+        
+        print("addFavorites button pressed")
+        
+        // update star
+        if navigationItem.rightBarButtonItem?.image == unfilledStar {
+            navigationItem.rightBarButtonItem!.image = filledStar
+        } else {
+            navigationItem.rightBarButtonItem!.image = unfilledStar
+        }
+        
+        // create detailItem object
+        //        var detailItem = [String:String]()
+        //        detailItem["title"] = movietitle
+        //        detailItem["vote_average"] = voteAverage
+        //        detailItem["poster_path"] = posterPath
+        //        detailItem["overview"] = plotOverview
+        
+        // add or remove in favorites
+        if isCurrentMovieInFavorites() {
+            print("removing from favorites")
+            // remove from favorites
+            favoritesArray.removeAtIndex(favMovieIndex())
+            
+        } else {
+            print("adding to favorites")
+            // add to favorites
+            favoritesArray.append(passedDetailItem)
+        }
+        
+        // re-add the array to nsuserdefaults
+        defaults.setObject(favoritesArray, forKey: "favoritesArray")
+        
+        print(favoritesArray.count)
+    }
+    
+    // checks if movie on the page is in the favorites array
+    func isCurrentMovieInFavorites() -> Bool {
+        
+        var bool = false
+        
+        let favoritesArray = defaults.valueForKey("favoritesArray") as! [[String:AnyObject]]
+        
+        if favoritesArray.count == 0 {
+            bool = false
+        }
+        
+        for savedFavorite in favoritesArray {
+            if savedFavorite["title"] as? String == movietitle {
+                bool = true
+            } else {
+                bool = false
+            }
+        }
+        
+        return bool
+    }
+    
+    // finds index of movie in favorites array assuming it exists - only call if isCurrentMovieInFavorites is true
+    func favMovieIndex() -> Int {
+        
+        var count = 1000
+        
+        let favoritesArray = defaults.valueForKey("favoritesArray") as! [[String:AnyObject]]
+        
+        for savedFavorite in favoritesArray {
+            count = 0
+            if savedFavorite["title"] as? String != movietitle {
+                count += 1
+            } else {
+                break
+            }
+        }
+        
+        return count
+    }
+}
+
--- TopTomatoes/TopTomatoes/MovieTableViewController.swift
+++ TopTomatoes/TopTomatoes/MovieTableViewController.swift
@@ -17,4 +17,36 @@ class MovieTableViewController: BaseTableViewController {
         url = "https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2016-01-01&api_key=66a3c05ffe0ce8bbd8985c438c018a0a&primary_release_date.lte=2016-02-25&language=en"
 
     }
+    
+    /// Segue function
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        let movieDetailController = segue.destinationViewController as! MovieDetailinScrollViewController
+        
+        // get the cell that generated the segue
+        if let selectedMovieCell = sender as? MovieTableViewCell {
+            let indexPath = tableView.indexPathForCell(selectedMovieCell)!
+            let selectedMovie = movieResponse[indexPath.row]
+            
+            
+            // build the url string
+            var imageUrlStub = selectedMovie["poster_path"]! as? String
+            
+            if imageUrlStub == nil {
+                imageUrlStub = ""
+            }
+            
+            let logoimageURL = "http://image.tmdb.org/t/p/w185" + imageUrlStub!
+            
+            //populate the next view
+            movieDetailController.movietitle = selectedMovie["title"] as? String
+            movieDetailController.movieimage = selectedMovieCell.thumbImage.image
+            movieDetailController.imageUrl = logoimageURL
+            movieDetailController.plotOverview = selectedMovie["overview"] as? String
+            movieDetailController.voteAverage = selectedMovie["vote_average"] as? String
+            movieDetailController.posterPath = selectedMovie["poster_path"] as? String
+            
+            movieDetailController.passedDetailItem = selectedMovie as [String:AnyObject]
+        }
+    }
 }
